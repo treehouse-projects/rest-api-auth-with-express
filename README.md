24b09c1985267dc55794855f70ba230ba24b8fdd
James Churchill <james@smashdev.com>
2018-11-15 15:50:31 -0800

    Added initial project files

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# rest-api-auth-with-express
+
+# REST API Authentication with Express
---
+++ app.js
@@ -0,0 +1,46 @@
+'use strict';
+
+// load modules
+const express = require('express');
+const morgan = require('morgan');
+
+// create the Express app
+const app = express();
+
+// setup request body JSON parsing
+app.use(express.json());
+
+// setup morgan which gives us http request logging
+app.use(morgan('dev'));
+
+// setup a friendly greeting for the root route
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Welcome to the REST API Authentication with Express project!',
+  });
+});
+
+// send 404 if no other route matched
+app.use((req, res) => {
+  res.status(404).json({
+    message: 'Route Not Found',
+  });
+});
+
+// setup a global error handler
+app.use((err, req, res, next) => {
+  console.error(`Global error handler: ${JSON.stringify(err.stack)}`);
+
+  res.status(500).json({
+    message: err.message,
+    error: process.env.NODE_ENV === 'production' ? {} : err,
+  });
+});
+
+// set our port
+app.set('port', process.env.PORT || 5000);
+
+// start listening on our port
+const server = app.listen(app.get('port'), () => {
+  console.log(`Express server is listening on port ${server.address().port}`);
+});
