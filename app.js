8a06bf24aff2856200ebb36ccf5fc700ea5d27bb
James Churchill <james@smashdev.com>
2018-11-16 14:16:43 -0800

    First pass at a completed example

--- app.js
+++ app.js
@@ -1,33 +1,36 @@
 'use strict';
 
-// load modules
 const express = require('express');
 const morgan = require('morgan');
+const routes = require('./routes');
 
-// create the Express app
+// Create the Express app.
 const app = express();
 
-// setup request body JSON parsing
+// Setup request body JSON parsing.
 app.use(express.json());
 
-// setup morgan which gives us http request logging
+// Setup morgan which gives us HTTP request logging.
 app.use(morgan('dev'));
 
-// setup a friendly greeting for the root route
+// Setup a friendly greeting for the root route.
 app.get('/', (req, res) => {
   res.json({
     message: 'Welcome to the REST API Authentication with Express project!',
   });
 });
 
-// send 404 if no other route matched
+// Add routes.
+app.use('/api', routes);
+
+// Send 404 if no other route matched.
 app.use((req, res) => {
   res.status(404).json({
     message: 'Route Not Found',
   });
 });
 
-// setup a global error handler
+// Setup a global error handler.
 app.use((err, req, res, next) => {
   console.error(`Global error handler: ${JSON.stringify(err.stack)}`);
 
@@ -37,10 +40,10 @@ app.use((err, req, res, next) => {
   });
 });
 
-// set our port
+// Set our port.
 app.set('port', process.env.PORT || 5000);
 
-// start listening on our port
+// Start listening on our port.
 const server = app.listen(app.get('port'), () => {
   console.log(`Express server is listening on port ${server.address().port}`);
 });
---
+++ routes.js
@@ -0,0 +1,122 @@
+'use strict';
+
+const express = require('express');
+const { check, validationResult } = require('express-validator/check');
+const bcryptjs = require('bcryptjs');
+const atob = require('atob');
+// const auth = require('basic-auth');
+
+// This array is used to keep track of user records
+// as they are created.
+const users = [];
+
+/**
+ * Returns the user's credentials from the Request Authorization header.
+ * @param {Request} req - The Express Request object.
+ */
+const getCredentials = (req) => {
+  let credentials = null;
+
+  const authorizationHeaderValue = req.get('Authorization');
+
+  if (authorizationHeaderValue.startsWith('Basic ')) {
+    const base64Credentials = authorizationHeaderValue.slice(6);
+    const stringCredentials = atob(base64Credentials);
+    const partsCredentials = stringCredentials.split(':');
+
+    credentials = {
+      name: partsCredentials[0],
+      pass: partsCredentials[1],
+    };
+  }
+
+  return credentials;
+};
+
+/**
+ * Middleware to authenticate the request using Basic Authentication.
+ * @param {Request} req - The Express Request object.
+ * @param {Response} res - The Express Response object.
+ * @param {Function} next - The function to call to pass execution to the next middleware.
+ */
+const authenticateUser = (req, res, next) => {
+  let message = null;
+
+  // Get the user's credentials from the Authorization header.
+  const credentials = getCredentials(req);
+  // const credentials = auth(req);
+
+  if (credentials) {
+    // Look for a user whose `username` matches the credentials `name` property.
+    const user = users.find(u => u.username === credentials.name);
+
+    if (user) {
+      const authenticated = bcryptjs
+        .compareSync(credentials.pass, user.password);
+      if (authenticated) {
+        console.log(`Authentication successful for username: ${user.username}`);
+
+        // Store the user on the Request object.
+        req.currentUser = user;
+      } else {
+        message = `Authentication failure for username: ${user.username}`;
+      }
+    } else {
+      message = `User not found for username: ${credentials.name}`;
+    }
+  } else {
+    message = 'Auth header not found';
+  }
+
+  if (message) {
+    console.warn(message);
+    res.status(401).json({ message: 'Access Denied' });
+  } else {
+    next();
+  }
+};
+
+// Construct a router instance.
+const router = express.Router();
+
+// Route that returns the current authenticated user.
+router.get('/users', authenticateUser, (req, res) => {
+  const user = req.currentUser;
+  res.json({
+    name: user.name,
+    username: user.username,
+  });
+});
+
+// Route that creates a new user.
+router.post('/users', [
+  check('name')
+    .exists({ checkNull: true, checkFalsy: true })
+    .withMessage('Please provide a value for "name"'),
+  check('username')
+    .exists({ checkNull: true, checkFalsy: true })
+    .withMessage('Please provide a value for "username"'),
+  check('password')
+    .exists({ checkNull: true, checkFalsy: true })
+    .withMessage('Please provide a value for "password"'),
+], (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const errorMessages = errors.array().map(error => error.msg);
+    return res.status(400).json({ errors: errorMessages });
+  }
+
+  const user = req.body;
+
+  // Hash the new user's password (if available).
+  if (user.password) {
+    user.password = bcryptjs.hashSync(user.password, 10);
+  }
+
+  // Add the user to the `users` array.
+  users.push(user);
+
+  return res.status(201).location('/users').json();
+});
+
+module.exports = router;
