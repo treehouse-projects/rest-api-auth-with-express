eca74bcae90fcc0679ffd49ae9c6be4ee3581152
James Churchill <james@smashdev.com>
2019-01-11 12:08:49 -0800

    Added completed project files for the manual header parsing example

---
+++ completed-project-manual-header-parsing/routes.js
@@ -0,0 +1,127 @@
+'use strict';
+
+const express = require('express');
+const { check, validationResult } = require('express-validator/check');
+const bcryptjs = require('bcryptjs');
+const atob = require('atob');
+
+// This array is used to keep track of user records
+// as they are created.
+const users = [];
+
+/**
+ * Returns the user's credentials from the Request Authorization header.
+ * @param {Request} req - The Express Request object.
+ */
+const getCredentials = (req) => {
+  let credentials = null;
+
+  const authorizationHeaderValue = req.get('Authorization');
+
+  if (authorizationHeaderValue && authorizationHeaderValue.startsWith('Basic ')) {
+    const base64Credentials = authorizationHeaderValue.slice(6);
+    const stringCredentials = atob(base64Credentials);
+    const partsCredentials = stringCredentials.split(':');
+
+    credentials = {
+      name: partsCredentials[0],
+      pass: partsCredentials[1],
+    };
+  }
+
+  return credentials;
+};
+
+/**
+ * Middleware to authenticate the request using Basic Authentication.
+ * @param {Request} req - The Express Request object.
+ * @param {Response} res - The Express Response object.
+ * @param {Function} next - The function to call to pass execution to the next middleware.
+ */
+const authenticateUser = (req, res, next) => {
+  let message = null;
+
+  // Get the user's credentials from the Authorization header.
+  const credentials = getCredentials(req);
+
+  if (credentials) {
+    // Look for a user whose `username` matches the credentials `name` property.
+    const user = users.find(u => u.username === credentials.name);
+
+    if (user) {
+      const authenticated = bcryptjs
+        .compareSync(credentials.pass, user.password);
+      if (authenticated) {
+        console.log(`Authentication successful for username: ${user.username}`);
+
+        // Store the user on the Request object.
+        req.currentUser = user;
+      } else {
+        message = `Authentication failure for username: ${user.username}`;
+      }
+    } else {
+      message = `User not found for username: ${credentials.name}`;
+    }
+  } else {
+    message = 'Auth header not found';
+  }
+
+  if (message) {
+    console.warn(message);
+    res.status(401).json({ message: 'Access Denied' });
+  } else {
+    next();
+  }
+};
+
+// Construct a router instance.
+const router = express.Router();
+
+// Route that returns the current authenticated user.
+router.get('/users', authenticateUser, (req, res) => {
+  const user = req.currentUser;
+
+  res.json({
+    name: user.name,
+    username: user.username,
+  });
+});
+
+// Route that creates a new user.
+router.post('/users', [
+  check('name')
+    .exists({ checkNull: true, checkFalsy: true })
+    .withMessage('Please provide a value for "name"'),
+  check('username')
+    .exists({ checkNull: true, checkFalsy: true })
+    .withMessage('Please provide a value for "username"'),
+  check('password')
+    .exists({ checkNull: true, checkFalsy: true })
+    .withMessage('Please provide a value for "password"'),
+], (req, res) => {
+  // Attempt to get the validation result from the Request object.
+  const errors = validationResult(req);
+
+  // If there are validation errors...
+  if (!errors.isEmpty()) {
+    // Use the Array `map()` method to get a list of error messages.
+    const errorMessages = errors.array().map(error => error.msg);
+
+    // Return the validation errors to the client.
+    return res.status(400).json({ errors: errorMessages });
+  }
+
+  // Get the user from the request body.
+  const user = req.body;
+
+  // Hash the new user's password.
+  user.password = bcryptjs.hashSync(user.password);
+
+  // Add the user to the `users` array.
+  users.push(user);
+
+  // Set the status to 201 Created and end the response.
+  return res.status(201).end();
+});
+
+module.exports = router;
